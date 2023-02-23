932a41c2699b028ab89fed44bda60160622fccd4
blacktyger <blacktyg3r@gmail.com>
2023-02-23 00:16:08 +0000

    start working on epicbox update

--- wallet/models.py
+++ wallet/models.py
@@ -6,8 +6,13 @@ from pydantic import BaseModel, Field
 
 from .. import utils
 
-EPICBOX_DOMAIN = 'epicpost.stackwallet.com'
+EPICBOX_DOMAIN = 'epic.tech'
 EPICBOX_PORT = 0
+
+
+class Account(BaseModel):
+    id: int | None = Field(default=0)
+    name: str | None = Field(default='default')
 
 
 class WalletConfig(utils.TOMLConfig):
@@ -22,58 +27,37 @@ class WalletConfig(utils.TOMLConfig):
         self.logging = self.settings['logging']
         self.password = password
         self.wallet_dir = wallet_dir
-        self.account: str = 'default'
-        self.account_id: int = 0
+        self.accounts: list = [Account(id=0, name='default')]
 
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def as_json(self):
-        return json.dumps({
-            'chain': self.wallet['chain_type'].lower(),
-            'account': self.account,
-            'wallet_dir': self.wallet_dir,
-            'check_node_api_http_addr': self.wallet['check_node_api_http_addr'],
-            'api_listen_interface': self.wallet['api_listen_interface'],
-            'api_listen_port': self.wallet['owner_api_listen_port'],
-            })
-
-    def essential(self, pass_path: str):
-        # For security reasons 'password' key is not password itself
-        # but path to encrypted file
-        return {'password': pass_path, 'wallet_dir': self.wallet_dir}
-
     def __str__(self):
-        return f"WalletConfig(path='{self.config_file}')"
+        return f"WalletConfig({self.wallet_dir})"
 
 
 class EpicBoxConfig(BaseModel):
+    address: str | None = Field(default='')
     prefix: str | None = Field('epicbox')
     domain: str | None = Field(default=EPICBOX_DOMAIN)
     port: str | None = Field(default=EPICBOX_PORT)
-    address: str | None = Field(default='')
     full_address: str | None
-    api_url: str | None
 
     def __init__(self, **data: Any):
         super().__init__(**data)
         self.init_config()
 
     def init_config(self):
-        _full_address = f"{self.prefix}://{self.address}@{self.domain}"
+        _full_address = f"{self.address}@{self.prefix}.{self.domain}"
 
         if self.port:
-            _full_address += f"{_full_address}:{self.port}"
+            _full_address = f"{_full_address}:{self.port}"
 
         self.full_address = _full_address
-        self.api_url = f"https://{self.domain}"
 
     def get_short_address(self):
         return f"{self.address[0:4]}...{self.address[-4:]}"
 
-    def as_json(self):
-        return self.json(include={'domain', 'port'})
-
     def __str__(self):
-        return f"EpicBox(address='{self.address}')"
+        return f"EpicBox({self.address})"
 
