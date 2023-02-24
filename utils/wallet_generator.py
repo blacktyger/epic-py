4f90cd0640c9fb837ac16bc1fe4e81417651f4de
blacktyger <blacktyg3r@gmail.com>
2023-02-24 18:49:13 +0000

    wallet egenrator script

--- utils/wallet_generator.py
+++ utils/wallet_generator.py
@@ -1,12 +1,11 @@
-import os
 import subprocess
-import uuid
-import shutil
-
 import platform
-from pydantic import BaseModel, Field
+import shutil
+import json
+import uuid
+import os
 
-from wallet.epic_sdk.wallet import Wallet
+from pydantic import BaseModel, Field
 
 
 if 'windows' in platform.system().lower():
@@ -14,23 +13,46 @@ if 'windows' in platform.system().lower():
 else:
     HOME_DIR = '~'
 
+WALLET_DATA_DIRECTORY = './wallets'
+SOURCE_PATH = "."
 PUBLIC_NODE = 'https://epic-radar.com/node'
+BINARY_NAME = 'epic-wallet'
+PASSWORD = 'majkut11'
+NETWORK = 'mainnet'
 
 
-class CreateWalletModel(BaseModel):
-    source_path: str | None = Field(default=os.getcwd())
-    description: str | None = Field(default='')
-    binary_name: str | None = Field(default='epic-wallet')
-    wallet_id: str | None = Field(default=uuid.uuid4())
-    password: str | None = Field(...)
-    wallet_name: str | None = Field(default=f'wallet_{wallet_id}')
-    network: str | None = Field(default='mainnet')
-    wallet_data_directory: str | None = Field(default=f'{HOME_DIR}/.epic/{network}/{binary_name}s/{wallet_name}')
+class WalletModel:
+    network: str | None
+    password: str | None
+    wallet_id: str | None = uuid.uuid4()
+    source_path: str | None
+    description: str | None = ''
+    binary_name: str | None
+    wallet_name: str | None = f'wallet_{wallet_id}'
+    wallet_data_directory: str = None
+
+    def __init__(self, **kwargs):
+        required_args = ('network', 'password', 'source_path', 'binary_name')
+
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+        if not self.wallet_data_directory:
+            self.wallet_data_directory = os.path.join(os.getcwd(), self.wallet_name)
+
+        if not all(arg in kwargs for arg in required_args):
+            raise SystemExit(f'Missing required argument/s, provide all: {required_args}') from None
+
+    def json(self):
+        d = self.__dict__.copy()
+        return json.dumps(d)
 
 
 def create(**kwargs):
-    model = CreateWalletModel(**kwargs)
-    source_full_path = f'{model.source_path}/{model.binary_name}'
+    model = WalletModel(**kwargs)
+    print(model.json())
+
+    source_full_path = os.path.join(model.source_path, model.binary_name)
 
     if not os.path.isfile(source_full_path):
         raise SystemExit(f'Invalid source {source_full_path} binary file path') from None
@@ -43,13 +65,19 @@ def create(**kwargs):
     pass_ = f"{model.password}\n".encode()
     args = f"./{model.binary_name} -r {PUBLIC_NODE} init -h"
 
-    init_wallet = subprocess.Popen(args.split(' '), stdin=subprocess.PIPE)
+    init_wallet = subprocess.Popen(args.split(' '), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
     # Provide pass as input, not argument (security)
     init_wallet.communicate(input=pass_)
-    init_wallet.communicate(input=pass_)
+    # init_wallet.communicate(input=pass_)
 
 
 if __name__ == '__main__':
-    # create(source_path='/home/blacktyger/epic-wallet/target/release', password='majkut11')
-    print(os.path.isdir("/home/blacktyger/epic-wallet/target/release/"))
+    create(
+        network=NETWORK,
+        binary_name=BINARY_NAME,
+        source_path=SOURCE_PATH,
+        password=PASSWORD
+        )
+    # print(os.getcwd())
+    # print(os.path.isdir(r"/home/blacktyger/epic-wallet/"))
